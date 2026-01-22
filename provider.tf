d008b9433d70ff58ddc033100ba370a1d4c4f6c4
Rahmathsd <mdrahmathalisd@gmail.com>
2026-01-22 18:14:30 +0530

    Formating

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
