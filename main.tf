a3b3db528ac15f4bcf1e55510cfc4050c497486e
Rahmathsd <mdrahmathalisd@gmail.com>
2026-01-22 18:30:54 +0530

    Debugging main

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "terra-key-ec2"
-  public_key = file("terra-key.pub")
-}
-
 resource "aws_instance" "test_ec2" {
     
-  key_name = aws_key_pair.deployer.key_name
     
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.micro"
